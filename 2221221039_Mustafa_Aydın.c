e3156e9cb6add6a13d51583c2740d9ae750a70d8
Mustafa_Aydin <mustafamulti12@gmail.com>
2025-11-30 22:10:07 +0300

    Gerekli kütüphaneler eklendi

--- 2221221039_Mustafa_Aydın.c
+++ 2221221039_Mustafa_Aydın.c
@@ -1,4 +1,20 @@
-#include <stdio.h>
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <string.h>     
+#include <time.h>       
+#include <sys/types.h>  
+#include <semaphore.h>  
+#include <fcntl.h>      
+#include <sys/mman.h>   
+#include <sys/stat.h>   
+#include <sys/ipc.h>    
+#include <sys/msg.h>    
+#include <unistd.h>     
+#include <signal.h>     
+#include <pthread.h>    
+#include <sys/wait.h>   
+#include <errno.h>      
+#include <stdarg.h>   
 
 int main (){
     printf("Deneme");
